4830d78df50ea30b6e5a7d0f19f52d5c2708deb3
darrenjon <darrenlin23@gmail.com>
2022-07-23 11:05:35 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // require packages used in the project
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// get mongoose connection status
+const db = mongoose.connection
+// connection fails
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// connection successful
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // routes setting
 app.get('/', (req, res) => {
