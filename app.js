1d3ad787ca0ec44809edc6f4fee011df74309f4f
darrenjon <darrenlin23@gmail.com>
2022-08-07 11:01:16 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -17,6 +18,6 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 // start and listen on the Express server
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
