774dd108503bc93e236c479dcf354d38d4384529
darrenjon <darrenlin23@gmail.com>
2022-08-20 17:15:03 +0800

    feat: add message partials

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ module.exports = {
     if (req.isAuthenticated()) {
       return next() //如果 req.isAuthenticated() 回傳 true，則執行下一個 middleware
     }
+    req.flash('warning_msg', '請先登入才能使用！') 
     res.redirect('/users/login') // false，就強制返回 login 頁面
   }
 }
